71147e53138a25c3b72ae103bfa94174fee5a2b3
Eddy Hernandez <eddy.josafat@basekit.com>
2017-04-03 13:18:24 +0100

    Fix typo

--- gene.pl
+++ gene.pl
@@ -1,25 +1,25 @@
 % A genealogical intelligent logigbase
 
-male(Bruce).
-male(John).
-male(Ernest).
-male(Chris).
-male(Jeff).
-male(Eddy).
+male('Bruce').
+male('John').
+male('Ernest').
+male('Chris').
+male('Jeff').
+male('Eddy').
 
-female(Joan).
-female(Tina).
-female(Rose).
-female(Monica).
-female(Faith).
+female('Joan').
+female('Tina').
+female('Rose').
+female('Monica').
+female('Faith').
 
-parent(Bruce, Eddy).
-parent(Bruce, Rose).
-parent(Bruce, Monica).
-parent(Faith, Eddy).
-parent(Faith, Rose).
-parent(Faith, Monica).
-parent(Jeff, Faith).
-parent(Joan, Faith).
-parent(Ernest, Chris).
-parent(Monica, Chris).
+parent('Bruce', 'Eddy').
+parent('Bruce', 'Rose').
+parent('Bruce', 'Monica').
+parent('Faith', 'Eddy').
+parent('Faith', 'Rose').
+parent('Faith', 'Monica').
+parent('Jeff', 'Faith').
+parent('Joan', 'Faith').
+parent('Ernest', 'Chris').
+parent('Monica', 'Chris').
